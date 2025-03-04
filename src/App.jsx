c0644df71d62bf72d99eaf1ee900da275699d81f
halaluddin2000 <helaluddin7001@gmail.com>
2025-03-04 15:49:49 +0600

    all page or compailer added

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import Order from "./Pages/Order";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Product from "./Pages/Product";
+import Navbar from "./Components/Navbar";
+function App() {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/placeOrder" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div>
+      <h2>hlw</h2>
+    </div>
+  );
+}
+
+export default Navbar;
