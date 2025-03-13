19bc2f0497bd229e6aa079f3c652b4eba6691787
halaluddin2000 <helaluddin7001@gmail.com>
2025-03-14 03:14:15 +0600

    best selles added

--- src/Components/BestSeller.jsx
+++ src/Components/BestSeller.jsx
@@ -25,15 +25,15 @@ const BestSeller = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {bestSeller.map((item, index) => {
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
             id={item._id}
             image={item.image}
             name={item.name}
             price={item.price}
-          />;
-        })}
+          />
+        ))}
       </div>
     </div>
   );
